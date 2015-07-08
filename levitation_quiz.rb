d98d8395f3b43addeeed8551d68e4371babc4bae
mbrickenworthe <adamwinters456@gmail.com>
2015-07-08 19:48:18 -0400

    Spell casting

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
-	#your code here
+  answer = gets.chomp
+	loop do
+    puts "What is the spell that enacts levitation?"
+    break if answer == "Wingardium Leviosa"
+  end
+  puts "You passed the quiz!"
 end
 
 
